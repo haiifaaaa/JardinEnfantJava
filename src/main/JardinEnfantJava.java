1813e49e5f64e12ed2d968016bff13ffc4ee7315
sedki <kmantersedki@hotmail.fr>
2020-06-06 16:54:01 +0200

    add gitignore file Rename main directory

--- src/main/JardinEnfantJava.java
+++ src/main/JardinEnfantJava.java
@@ -3,17 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jardinenfantjava;
+package main;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
